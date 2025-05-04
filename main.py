75e3b2e6cf8e9f440afc39aa34e20f95196280d6
kaizo <kaizo.nkg@gmail.com>
2025-05-04 15:19:00 +0700

    disbale tflite

--- main.py
+++ main.py
@@ -64,21 +64,21 @@ with open(PATH_TO_LABELS, "r") as f:
 if labels[0] == "???":
     del labels[0]
 
-pkg = importlib.util.find_spec("tflite_runtime")
-if pkg:
-    from tflite_runtime.interpreter import Interpreter
-else:
-    from tensorflow.lite.python.interpreter import Interpreter
+# pkg = importlib.util.find_spec("tflite_runtime")
+# if pkg:
+#     from tflite_runtime.interpreter import Interpreter
+# else:
+#     from tensorflow.lite.python.interpreter import Interpreter
 
-interpreter = Interpreter(model_path=PATH_TO_CKPT)
-interpreter.allocate_tensors()
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-height = input_details[0]["shape"][1]
-width = input_details[0]["shape"][2]
-floating_model = input_details[0]["dtype"] == np.float32
-input_mean = 127.5
-input_std = 127.5
+# interpreter = Interpreter(model_path=PATH_TO_CKPT)
+# interpreter.allocate_tensors()
+# input_details = interpreter.get_input_details()
+# output_details = interpreter.get_output_details()
+# height = input_details[0]["shape"][1]
+# width = input_details[0]["shape"][2]
+# floating_model = input_details[0]["dtype"] == np.float32
+# input_mean = 127.5
+# input_std = 127.5
 
 # ----- Global Variables -----
 global_url = None
@@ -95,78 +95,78 @@ global_save_image = False
 global_modelname = "yolo"
 # --- End Global Variables ----
 
-outname = output_details[0]["name"]
+# outname = output_details[0]["name"]
 
-if "StatefulPartitionedCall" in outname:  # This is a TF2 model
-    boxes_idx, classes_idx, scores_idx = 1, 3, 0
-else:  # This is a TF1 model
-    boxes_idx, classes_idx, scores_idx = 0, 1, 2
+# if "StatefulPartitionedCall" in outname:  # This is a TF2 model
+#     boxes_idx, classes_idx, scores_idx = 1, 3, 0
+# else:  # This is a TF1 model
+#     boxes_idx, classes_idx, scores_idx = 0, 1, 2
 
 
-def tflite_detect(frame):
-    image = frame.to_ndarray(format="bgr24")
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    imH, imW, _ = image.shape
-    image_resized = cv2.resize(image_rgb, (width, height))
-    input_data = np.expand_dims(image_resized, axis=0)
+# def tflite_detect(frame):
+#     image = frame.to_ndarray(format="bgr24")
+#     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#     imH, imW, _ = image.shape
+#     image_resized = cv2.resize(image_rgb, (width, height))
+#     input_data = np.expand_dims(image_resized, axis=0)
 
-    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-    if floating_model:
-        input_data = (np.float32(input_data) - input_mean) / input_std
+#     # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+#     if floating_model:
+#         input_data = (np.float32(input_data) - input_mean) / input_std
 
-    # Perform the actual detection by running the model with the image as input
-    interpreter.set_tensor(input_details[0]["index"], input_data)
-    interpreter.invoke()
+#     # Perform the actual detection by running the model with the image as input
+#     interpreter.set_tensor(input_details[0]["index"], input_data)
+#     interpreter.invoke()
 
-    # Retrieve detection results
-    boxes = interpreter.get_tensor(output_details[boxes_idx]["index"])[
-        0
-    ]  # Bounding box coordinates of detected objects
-    classes = interpreter.get_tensor(output_details[classes_idx]["index"])[
-        0
-    ]  # Class index of detected objects
-    scores = interpreter.get_tensor(output_details[scores_idx]["index"])[
-        0
-    ]  # Confidence of detected objects
+#     # Retrieve detection results
+#     boxes = interpreter.get_tensor(output_details[boxes_idx]["index"])[
+#         0
+#     ]  # Bounding box coordinates of detected objects
+#     classes = interpreter.get_tensor(output_details[classes_idx]["index"])[
+#         0
+#     ]  # Class index of detected objects
+#     scores = interpreter.get_tensor(output_details[scores_idx]["index"])[
+#         0
+#     ]  # Confidence of detected objects
 
-    detections = []
-    for i in range(len(scores)):
-        if (scores[i] > CONF_THRESHOLD) and (scores[i] <= 1.0):
-            ymin = int(max(1, (boxes[i][0] * imH)))
-            xmin = int(max(1, (boxes[i][1] * imW)))
-            ymax = int(min(imH, (boxes[i][2] * imH)))
-            xmax = int(min(imW, (boxes[i][3] * imW)))
-            cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
+#     detections = []
+#     for i in range(len(scores)):
+#         if (scores[i] > CONF_THRESHOLD) and (scores[i] <= 1.0):
+#             ymin = int(max(1, (boxes[i][0] * imH)))
+#             xmin = int(max(1, (boxes[i][1] * imW)))
+#             ymax = int(min(imH, (boxes[i][2] * imH)))
+#             xmax = int(min(imW, (boxes[i][3] * imW)))
+#             cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
 
-            # Draw label
-            object_name = labels[
-                int(classes[i])
-            ]  # Look up object name from "labels" array using class index
-            label = "%s: %d%%" % (object_name, int(scores[i] * 100))
-            labelSize, baseLine = cv2.getTextSize(
-                label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2
-            )  # Get font size
-            label_ymin = max(
-                ymin, labelSize[1] + 10
-            )  # Make sure not to draw label too close to top of window
-            cv2.rectangle(
-                image,
-                (xmin, label_ymin - labelSize[1] - 10),
-                (xmin + labelSize[0], label_ymin + baseLine - 10),
-                (255, 255, 255),
-                cv2.FILLED,
-            )
-            cv2.putText(
-                image,
-                label,
-                (xmin, label_ymin - 7),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.7,
-                (0, 0, 0),
-                2,
-            )
-            detections.append(object_name)
-    return [image, detections]
+#             # Draw label
+#             object_name = labels[
+#                 int(classes[i])
+#             ]  # Look up object name from "labels" array using class index
+#             label = "%s: %d%%" % (object_name, int(scores[i] * 100))
+#             labelSize, baseLine = cv2.getTextSize(
+#                 label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2
+#             )  # Get font size
+#             label_ymin = max(
+#                 ymin, labelSize[1] + 10
+#             )  # Make sure not to draw label too close to top of window
+#             cv2.rectangle(
+#                 image,
+#                 (xmin, label_ymin - labelSize[1] - 10),
+#                 (xmin + labelSize[0], label_ymin + baseLine - 10),
+#                 (255, 255, 255),
+#                 cv2.FILLED,
+#             )
+#             cv2.putText(
+#                 image,
+#                 label,
+#                 (xmin, label_ymin - 7),
+#                 cv2.FONT_HERSHEY_SIMPLEX,
+#                 0.7,
+#                 (0, 0, 0),
+#                 2,
+#             )
+#             detections.append(object_name)
+#     return [image, detections]
 
 
 # ----- End TensorFlow Lite -----
